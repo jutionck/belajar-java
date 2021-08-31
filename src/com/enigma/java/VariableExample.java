3e69ed56be7d2ca9d353c2cba8d740f7c41edbd9
Jution Candra Kirana <jutionck@gmail.com>
2021-08-31 23:45:38 +0700

    #3 Variable - Java Course

---
+++ src/com/enigma/java/VariableExample.java
@@ -0,0 +1,25 @@
+package com.enigma.java;
+
+public class VariableExample {
+
+    public static void main(String[] args) {
+
+        String bootcamp = "Enigma Camp";
+
+        String _bankName = "Bank Negara Indonesia";
+
+        String $_universityName = "Universitas Gadjah Mada";
+
+        int bootcampAge = 2;
+
+        double phi = 3.14;
+
+        // since java 10, java can use key Var
+        // var bootcampAddress = "Jl. H Dahlan No. 75 Jakarta Selatan";
+
+        // final
+        final  String bootcampAddress = "Jl. H Dahlan No. 75 Jakarta Selatan";
+        // bootcampAddress = "Can't assign";
+
+    }
+}
