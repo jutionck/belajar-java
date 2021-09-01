10c6af1007a262d79ea5e409814fba4e62e3a05c
Jution Candra Kirana <jutionck@gmail.com>
2021-09-01 22:28:03 +0700

    #10 Looping Array - Java Course

---
+++ src/com/enigma/java/ExampleArray.java
@@ -0,0 +1,81 @@
+package com.enigma.java;
+
+import java.util.Scanner;
+
+public class ExampleArray {
+
+    public static void main(String[] args) {
+
+        /**
+         * Apa yang akan  dilakukan apabila kita akan menyimpan data lebih dari 1 ?
+         * Dibawah ini contoh nya :
+         */
+
+//        String member = "Jack";
+//        String member2 = "Sparrow";
+//        String member3 = "Lina";
+//        String member4 = "Brad";
+//        String memebr5 = "Angeline";
+
+        /**
+         * Bayangkan jika data sudah mulai banyak ?
+         * Maka muncul lah Array.
+         * Array adalah sebuah variable tempat menyimpan data yang lebih dari 1, seperti di atas contohnya
+         */
+
+        // cara membuat sebuah array terdapat 3 macam :
+
+        String[] members = {"Jack", "Sparrow", "Lina", "Brad", "Angeline"};
+//        System.out.println(members[0]);
+//        System.out.println(members[1]);
+//        System.out.println(members[2]);
+//        System.out.println(members[3]);
+//        System.out.println(members[4]);
+
+        String anotherMembers[] = {"Jack", "Sparrow", "Lina", "Brad", "Angeline"};
+
+        String[] anotherMember = new String[5]; // index 0 - 4 | Isi array 5
+        anotherMember[0] = "Jack";
+        anotherMember[1] = "Sparrow";
+        anotherMember[2] = "Lina";
+        anotherMember[3] = "Brad";
+        anotherMember[4] = "Angeline";
+
+        // menggunakan for
+//        for (int i = 0; i < anotherMember.length; i++) {
+//            System.out.println("Member ke-" + i + " " + anotherMember[i]);
+//        }
+
+        /**
+         * Pertanyaan nya bagaimana cara untuk menampilkan semua isi sebuah array tanpa mencetak satu persatu
+         * Bisa menggunakan sebuah for atau forEach
+         */
+
+        // forEach
+//        for (String member: members) {
+//            System.out.println(member);
+//        }
+
+        /**
+         * Buat lah sebuah variable languageProgramming yang di dapat menyimpan 5 buah bahasa pemrograman
+         * Data yang disimpan berasal dari inputan
+         */
+
+        // Jawaban
+        Scanner scanner = new Scanner(System.in);
+
+        String languageProgrammings[] = new String[5];
+
+        for (int i = 0; i < languageProgrammings.length; i++) {
+            System.out.print("Bahasa pemrograman ke-" + i + ": ");
+            languageProgrammings[i] = scanner.nextLine();
+        }
+
+        System.out.println("-------------------------------------");
+
+        for (String item:languageProgrammings) {
+            System.out.println(item);
+        }
+
+    }
+}
---
+++ src/com/enigma/java/ExampleArrayMulti.java
@@ -0,0 +1,53 @@
+package com.enigma.java;
+
+import java.util.Scanner;
+
+public class ExampleArrayMulti {
+
+    public static void main(String[] args) {
+
+//        String[][]  members = {
+//                {"Jack", "+62721"},
+//                {"Mila", "+67262"},
+//                {"Robert", "+72821"}
+//        };
+//
+//        // pemanggilan cara pertama
+//        for (int i = 0; i < members.length; i++) {
+//            System.out.println("Name: " + members[i][0]);
+//            System.out.println("Contact: " + members[i][1]);
+//            System.out.println("---------------------------");
+//        };
+//
+//        // pemanggilan cara kedua menggunakan forEach
+//        for (String[] member : members) {
+//            System.out.println("Name: " + member[0]);
+//            System.out.println("Contact: " + member[1]);
+//            System.out.println("---------------------------");
+//        }
+
+        /**
+         * Buat lah sebuah variabel array 2 dimensi yang menyimpan data customer (nama dan kota asal)
+         */
+
+        String[][] customers = new String[2][3];
+        Scanner scanner = new Scanner(System.in);
+
+        for (int row = 0; row < customers.length; row++) {
+            for (int col = 0; col < customers[row].length; col++) {
+                System.out.printf("Siapa nama customer yang akan di inputkan [%d, %d]: ", row, col);
+                customers[row][col] = scanner.nextLine();
+            }
+        }
+
+        System.out.println("-------------------------------------");
+        for (int row = 0; row < customers.length; row++) {
+            for (int col = 0; col < customers[row].length; col++) {
+                System.out.format("| %s | \t", customers[row][col]);
+            }
+            System.out.println("");
+        }
+
+        System.out.println("-------------------------------------");
+    }
+}
